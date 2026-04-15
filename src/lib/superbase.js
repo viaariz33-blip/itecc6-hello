8fb45c21c926f8dfa558ceb7a049e2661d17021f
ARIZ, VIA A. <viaariz33@gmail.com>
2026-04-15 09:01:37 +0800

    Update superbase.js
    "fix supabse key"

--- src/lib/superbase.js
+++ src/lib/superbase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nmalnczybxjaltdmztya.supabase.co'
-const supabaseKey = 'ILAGAY DITO ANG ANON KEY NA NA-COPY MO'
+const supabaseKey = 'sb_publishable_3xdkig8v2YJfMAOA4AnPEQ_5PL74jxl'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
